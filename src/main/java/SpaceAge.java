25cacd063e887c3858b4c74eba5513b2a3575d05
nickl0gist <nicklogistick@mail.ru>
2019-06-15 12:21:00 +0200

    Space Age

--- src/main/java/SpaceAge.java
+++ src/main/java/SpaceAge.java
@@ -1,1 +1,45 @@
 
+public class SpaceAge{
+
+    long age;
+
+    public SpaceAge(int age) {
+        this.age = age;
+    }
+
+    public SpaceAge(long l) {
+        this.age = l;
+    }
+
+    public double onEarth() {
+        return age / 31557600.0;
+    }
+
+    public double onMercury() {
+        return onEarth()/0.2408467;
+    }
+
+    public double onVenus() {
+        return onEarth()/0.61519726;
+    }
+
+    public double onMars() {
+        return onEarth()/1.8808158;
+    }
+
+    public double onJupiter() {
+        return onEarth()/11.862615;
+    }
+
+    public double onSaturn() {
+        return onEarth()/29.447498;
+    }
+
+    public double onUranus() {
+        return onEarth()/84.016846;
+    }
+
+    public double onNeptune() {
+        return onEarth()/164.79132;
+    }
+}
--- src/test/java/SpaceAgeTest.java
+++ src/test/java/SpaceAgeTest.java
@@ -10,70 +10,48 @@ public class SpaceAgeTest {
     @Test
     public void ageOnEarth() {
         SpaceAge age = new SpaceAge(1000000000);
-
         assertEquals(31.69, age.onEarth(), MAXIMUM_DELTA);
     }
 
-
-    @Ingore
     @Test
     public void ageOnMercury() {
         SpaceAge age = new SpaceAge(2134835688);
-
         assertEquals(280.88, age.onMercury(), MAXIMUM_DELTA);
     }
 
-
-    @Ingore
     @Test
     public void ageOnVenus() {
         SpaceAge age = new SpaceAge(189839836);
-
         assertEquals(9.78, age.onVenus(), MAXIMUM_DELTA);
     }
 
-
-    @Ingore
     @Test
     public void ageOnMars() {
         SpaceAge age = new SpaceAge(2329871239L);
-
         assertEquals(39.25, age.onMars(), MAXIMUM_DELTA);
     }
 
-
-    @Ingore
     @Test
     public void ageOnJupiter() {
         SpaceAge age = new SpaceAge(901876382);
-
         assertEquals(2.41, age.onJupiter(), MAXIMUM_DELTA);
     }
 
-
-    @Ingore
     @Test
     public void ageOnSaturn() {
         SpaceAge age = new SpaceAge(3000000000L);
-
         assertEquals(3.23, age.onSaturn(), MAXIMUM_DELTA);
     }
 
-
-    @Ingore
     @Test
     public void ageOnUranus() {
         SpaceAge age = new SpaceAge(3210123456L);
-
         assertEquals(1.21, age.onUranus(), MAXIMUM_DELTA);
     }
 
-
-    @Ingore
     @Test
     public void ageOnNeptune() {
         SpaceAge age = new SpaceAge(8210123456L);
-
         assertEquals(1.58, age.onNeptune(), MAXIMUM_DELTA);
     }
 }
